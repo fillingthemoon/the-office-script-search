eb15dee8fa0a7e7db2fa8d4622850942bf8213f2
philemonheng <phil.hsy@outlook.com>
2021-05-20 19:20:48 +0800

    place episodes in table

--- client/src/components/Episodes.js
+++ client/src/components/Episodes.js
@@ -40,21 +40,49 @@ const Episodes = () => {
     }
   })
 
-  const dataSource = [{ key: '1' }]
-  episodes.forEach((season) => {
-    dataSource[0][`season${season._id}`] = (
-      season.lines.map((episode, i) =>
-        <Link
-          style={{ display: 'block' }}
-          key={i}
-          to={`/seasons/${season._id}/episodes/${episode._id}/lines`}
-          onClick={() => dispatch(setSeasonEpisodeScene([season._id, episode._id, null]))}
-        >
-          {`Ep. ${episode._id}: ${episode.episodeTitle}`}
-        </Link>
-      )
-    )
+  const episodesSimplified = episodes.map((season) => {
+    let obj = {}
+    season.lines.forEach((episode, i) => {
+      obj[episode._id] = episode.episodeTitle
+    })
+    return obj
   })
+
+  let episodesReordered = {}
+  Object.keys(episodesSimplified).forEach(seasonId => {
+    Object.keys(episodesSimplified[seasonId]).forEach(episodeId => {
+      const episodeObject = {
+        seasonId: Number(seasonId) + 1,
+        episodeTitle: episodesSimplified[seasonId][episodeId],
+      }
+      if (Object.keys(episodesReordered).includes(episodeId)) {
+        episodesReordered[episodeId].push(episodeObject)
+      } else {
+        episodesReordered[episodeId] = [episodeObject]
+      }
+    })
+  })
+
+  const dataSource = Object.keys(episodesReordered)
+    .map((episodeId, i) => {
+      // console.log(episodesReordered[episodeId])
+      const obj = { key: i + 1 }
+      Object.keys(episodesReordered[episodeId])
+        .map(seasonIdKey => { // Starts from 0!
+          const episodeObject = episodesReordered[episodeId][Number(seasonIdKey)]
+          obj[`season${episodeObject.seasonId}`] = (
+            <Link
+              style={{ display: 'block' }}
+              key={i}
+              to={`/seasons/${episodeObject.seasonId}/episodes/${episodeId}/lines`}
+              onClick={() => dispatch(setSeasonEpisodeScene([episodeObject.seasonId, episodeId, null]))}
+            >
+              {`Ep. ${episodeId} - ${episodeObject.episodeTitle}`}
+            </Link>
+          )
+        })
+      return obj
+    })
 
   return (
     <div>
